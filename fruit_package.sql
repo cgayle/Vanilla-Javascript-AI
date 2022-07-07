cf689793ed0f4afaba69890f7724aabba329b640
Gayle Bitong <gbitong@arcanys.com>
2022-07-07 14:05:47 +0800

    update database

--- fruit_package.sql
+++ fruit_package.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `fruit_package` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `fruit_package`;
 -- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
 --
 -- Host: localhost    Database: fruit_package
@@ -25,8 +27,9 @@ DROP TABLE IF EXISTS `fruits`;
 CREATE TABLE `fruits` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(45) DEFAULT NULL,
+  `price` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,6 +38,7 @@ CREATE TABLE `fruits` (
 
 LOCK TABLES `fruits` WRITE;
 /*!40000 ALTER TABLE `fruits` DISABLE KEYS */;
+INSERT INTO `fruits` VALUES (1,'apple','10'),(2,'pears','9'),(3,'green apple','10'),(4,'mandarin','12');
 /*!40000 ALTER TABLE `fruits` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,9 +53,9 @@ CREATE TABLE `sales` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fruit` varchar(45) DEFAULT NULL,
   `quantity_sold` int(11) DEFAULT NULL,
-  `date_sold` datetime DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -60,6 +64,7 @@ CREATE TABLE `sales` (
 
 LOCK TABLES `sales` WRITE;
 /*!40000 ALTER TABLE `sales` DISABLE KEYS */;
+INSERT INTO `sales` VALUES (2,'1',2,'2022-07-07 11:53:44'),(3,'2',3,'2022-06-06 12:56:26'),(4,'1',2,'2021-06-06 12:56:26'),(5,'3',1,'2021-06-06 12:56:26');
 /*!40000 ALTER TABLE `sales` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -72,10 +77,10 @@ DROP TABLE IF EXISTS `stocks`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `stocks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fruit` varchar(45) DEFAULT NULL,
-  `quantity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `fruit` int(11) NOT NULL,
+  `weight` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`,`fruit`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,8 +89,38 @@ CREATE TABLE `stocks` (
 
 LOCK TABLES `stocks` WRITE;
 /*!40000 ALTER TABLE `stocks` DISABLE KEYS */;
+INSERT INTO `stocks` VALUES (1,1,5),(2,2,4);
 /*!40000 ALTER TABLE `stocks` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `supplier`
+--
+
+DROP TABLE IF EXISTS `supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fruit` varchar(45) DEFAULT NULL,
+  `price` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supplier`
+--
+
+LOCK TABLES `supplier` WRITE;
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+INSERT INTO `supplier` VALUES (1,'1','8'),(2,'2','7'),(3,'3','8'),(5,'4',NULL);
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'fruit_package'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -96,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-07  2:54:09
+-- Dump completed on 2022-07-07 14:05:18
